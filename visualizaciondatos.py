37815e7e51b7953e29ea6891c127b8e41e28b55b
Diana Diaz <dcdiazgo@uoc.edu>
2025-12-03 22:15:07 +0100

    Update visualizaciondatos.py

--- visualizaciondatos.py
+++ visualizaciondatos.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
+st.set_page_config(page_title="Predicciones", page_icon="⚕", layout="wide")
+
+
 st.title("📊 Cuáles fueron los resultados?")
 st.write("Exploremos lo que respondieron los encuestados en España sobre su salud en 2023")
